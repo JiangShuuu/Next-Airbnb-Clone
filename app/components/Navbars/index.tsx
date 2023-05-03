87228308bb2a8e265af2990df9062ba0e45d5539
JiangShuuu <globelex65@gmail.com>
2023-05-03 22:02:04 +0800

    first

--- app/components/Navbars/index.tsx
+++ app/components/Navbars/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import Container from '../Container'
+import Logo from './Logo'
 
 export default function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <div className="fixed z-10 w-full bg-white shadow-sm">
+      <div className="py-4 border-b">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+          </div>
+        </Container>
+      </div>
+    </div>
+  )
 }
