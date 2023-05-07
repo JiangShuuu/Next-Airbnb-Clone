0903638d2a56815e35043e13c3ac54a73a5d90db
JiangShuuu <globelex65@gmail.com>
2023-05-07 11:29:13 +0800

    add button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 import Navbar from './components/Navbars'
+import Modals from './components/Modals'
+import ClientOnly from './components/ClientOnly'
 
 export const metadata = {
 	title: 'Airbnb',
@@ -10,10 +12,15 @@ export const metadata = {
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
+				<Modals actionLabel='Submit' isOpen title='hello' />
 				<Navbar />
 				{children}
 			</body>
