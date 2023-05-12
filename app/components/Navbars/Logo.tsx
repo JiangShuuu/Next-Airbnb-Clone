0147ce0e310ba3360b79d80704f70bf7f58ee8e7
JiangShuuu <globelex65@gmail.com>
2023-05-12 21:52:00 +0800

    upd icons

--- app/components/Navbars/Logo.tsx
+++ app/components/Navbars/Logo.tsx
@@ -5,14 +5,15 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
 export default function Logo() {
-  const router = useRouter()
-  return (
-    <Image
-      alt="Logo"
-      height="100"
-      width="100"
-      src="/images/logo.png"
-      className="hidden cursor-pointer md:block"
-    />
-  )
+	const router = useRouter()
+	return (
+		<Image
+			onClick={() => router.push('/')}
+			alt='Logo'
+			height='100'
+			width='100'
+			src='/images/logo.png'
+			className='hidden cursor-pointer md:block'
+		/>
+	)
 }
