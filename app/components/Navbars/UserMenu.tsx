2c2a074d74dad2a8ab289dea1938fab0bfba223d
JiangShuuu <globelex65@gmail.com>
2023-05-28 11:44:37 +0800

    add del properties

--- app/components/Navbars/UserMenu.tsx
+++ app/components/Navbars/UserMenu.tsx
@@ -77,7 +77,12 @@ export default function UserMenu({ currentUser }: UserMenuProps) {
 									}}
 									label='My reservations'
 								/>
-								<MenuItem onClick={() => {}} label='My properties' />
+								<MenuItem
+									onClick={() => {
+										router.push('/properties')
+									}}
+									label='My properties'
+								/>
 								<MenuItem
 									onClick={() => {
 										rentModal.onOpen()
---
+++ app/properties/PropertiesClient.tsx
@@ -0,0 +1,57 @@
+'use client'
+import React, { useCallback, useState } from 'react'
+import { SafeListing, SafeUser } from '../types'
+import Container from '../components/Container'
+import Heading from '../components/Heading'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import ListingCard from '../components/listings/ListingCard'
+interface PropertiesClientProps {
+	currentUser?: SafeUser | null
+	listings: SafeListing[]
+}
+
+export default function PropertiesClient({
+	currentUser,
+	listings,
+}: PropertiesClientProps) {
+	const router = useRouter()
+	const [deletingId, setDeletingId] = useState('')
+	const onCancel = useCallback(
+		(id: string) => {
+			setDeletingId(id)
+			axios
+				.delete(`/api/listings/${id}`)
+				.then(() => {
+					toast.success(`Listing deleted`)
+					router.refresh()
+				})
+				.catch((error) => {
+					toast.error(error?.response?.data?.error)
+				})
+				.finally(() => {
+					setDeletingId('')
+				})
+		},
+		[router],
+	)
+	return (
+		<Container>
+			<Heading title='Properties' subtitle='List of your preperties' />
+			<div className='grid grid-cols-1 gap-8 mt-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+				{listings.map((listing) => (
+					<ListingCard
+						key={listing.id}
+						data={listing}
+						actionId={listing.id}
+						onAction={onCancel}
+						disabled={deletingId === listing.id}
+						actionLabel='Delete property'
+						currentUser={currentUser}
+					/>
+				))}
+			</div>
+		</Container>
+	)
+}
